e656ef5d5b27fd114e2934ed053fed4190dd91b8
GabbisCode <gabbi.huang@outlook.com>
2022-03-20 20:50:42 +0800

    Uploaded my notes

--- C/challange/04-Print_the_byte_size_of_the_basic_data_types/04-Print_the_byte_size_of_the_basic_data_types.c
+++ C/challange/04-Print_the_byte_size_of_the_basic_data_types/04-Print_the_byte_size_of_the_basic_data_types.c
@@ -2,3 +2,18 @@
  * Author: Gabbi
  * Date: 2022/3/20
  **/
+
+#include <stdio.h>
+
+int main()
+{
+    //display the byte size of int, char, long, long long, double, long double
+    printf("The length of int is: %zd\n", sizeof(int));
+    printf("The length of char is: %zd\n", sizeof(char)); 
+    printf("The length of long is: %zd\n", sizeof(long)); 
+    printf("The length of long long is: %zd\n", sizeof(long long)); 
+    printf("The length of double is: %zd\n", sizeof(double)); 
+    printf("The length of long double is: %zd\n", sizeof(long double));  
+
+    return 0;
+}
